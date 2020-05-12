120749cb86d17a9f4547cd4dcec0d5f179ef44c0
Vera <vlz210@nyu.edu>
2020-05-12 12:21:20 -0400

    added persona cards + housekeeping

---
+++ assets/js/persona.js
@@ -0,0 +1,19 @@
+function displayManny() {
+    let manny2= document.getElementById('manny2');
+    manny2.style.visibility = 'visible';
+}
+
+function hideManny() {
+    let manny2 = document.getElementById('manny2');
+    manny2.style.visibility = 'hidden';
+}
+
+function displayAndy() {
+    let andy2= document.getElementById('andrea2');
+    andy2.style.visibility = 'visible';
+}
+
+function hideAndy() {
+    let andy2 = document.getElementById('andrea2');
+    andy2.style.visibility = 'hidden';
+}
